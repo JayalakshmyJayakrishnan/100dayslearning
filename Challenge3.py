07fcb9c14d61f4fdaafbdc17095c849ecd502744
Jayalakshmy Jayakrishnan <lakshmystudy21@gmail.com>
2024-07-01 11:47:23 -0700

    Create Challenge3.py
    //Print Right Angled Triangle//
    
    This program performs the execution of a pattern in the shape of a right angled triangle with a specified number of rows. 
    This pattern is achieved through nested loops, thus generating the required output using "*".

---
+++ Challenge3.py
@@ -0,0 +1,6 @@
+#Define the number of rows for the triangle
+n = 5
+for i in range(n):                  #Loop for each row
+    for j in range(i + 1):          #Loop for each coloumn in the present row   
+        print("*", end="")          #Print * without a newline
+    print()                         #Prints a newline at the end of each row
