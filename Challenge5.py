c32c0855336a56fe476ccf4bf868534624304dfa
Jayalakshmy Jayakrishnan <lakshmystudy21@gmail.com>
2024-07-02 02:30:30 -0700

    Challenge5.py
    //Convert Temperature//
    This python program converts the temperature in Celsius to Kelvin by initializing a temperature value in Celsius and converts it to Kelvin. It then prints the value in both Celsius and Kelvin using formatted string literals (f-strings).
    
    
    Formatted string literals, is a way to embed expressions inside string literals using curly braces '{}'.  
    Here,
    'f"Celsius-{temperature}"' embeds the 'temperature' variable in the string.
    'f"Kelvin-{kelvin}"' embeds the 'kelvin' variable in the string.
    
    These f-strings make the code more readable and concise.

---
+++ Challenge5.py
@@ -0,0 +1,13 @@
+#Declare and initialize the variable 'temperature' in Celcius
+temperature = 25.5 
+
+#Conversion to Kelvin
+kelvin = temperature + 273
+
+#Print the temperature in Celcius and Kelvin
+print(f"Celsius - {temperature}")
+print(f"Kelvin - {kelvin}")
+
+
+#Output obtained: Celsius - 25.5
+                  Kelvin - 298.5
