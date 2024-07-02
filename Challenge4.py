000f7cd4c43b0197bc31b34ad8f79dfd22590dd1
Jayalakshmy Jayakrishnan <lakshmystudy21@gmail.com>
2024-07-02 01:17:36 -0700

    Challenge4.py
    //Square of Stars//
    
    This program prints a square of '*' (stars) of size 4. 
    With the help of a for loop, it prints the required number of rows to form the square.

---
+++ Challenge4.py
@@ -0,0 +1,14 @@
+#Define the size of the square
+n = 4           
+
+for i in range(n):                    #Loop to print each row of the sqaure
+    print('*'*n)                      #Prints the required row of stars
+
+
+
+
+
+#Ouput obtained : ****
+                  ****
+                  ****
+                  ****
