48d956929a2a9cae6b82398968d898cb55589239
Jayalakshmy Jayakrishnan <lakshmystudy21@gmail.com>
2024-07-03 09:48:52 -0700

    Challenge8.py
    This program generates a random password of specified length 'passlen' using unique set of characters from a defined set  'x'. 
    
    Here, the code imports the random module, specifies the character set, sets the password length, and uses the function 'random.sample()' to select a unique set of characters from the set 'x', and joins them into a single string.
    
    # 'random.sample(x, passlen)' generates unique set of characters from the set 'x' without any repetition.
    
    #join() function is a string method which concatenates the elements of the list, (here, elements of x), thereby converting the list into a string.
    
    #"" before join() function is used as a separator to ensure that only characters from the given list is used to concatenate into a string, i.e., it avoids spaces between the joined characters.

---
+++ Challenge8.py
@@ -0,0 +1,14 @@
+import random
+
+_#Define the set of characters to choose from for the password_
+x = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()?><:"{}[]\|+_=-.,;~`' "
+
+_#Specify the desired length of the password_
+
+passlen = 14
+
+_# random.sample() creates a list of 'passlen' unique characters from 's_
+
+p = " ".join(random.sample(x, passlen))
+
+print(p)
