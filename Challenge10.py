f9bed02a8fec4fbf4050875190c22b824e059364
Jayalakshmy Jayakrishnan <lakshmystudy21@gmail.com>
2024-07-05 11:32:53 -0700

    Challenge10.py
    The code initializes a list of Fibonacci numbers and iterates through each element in the list. 
    For each element, it checks if the value is less than 5. 
    If the condition is met, it prints the value.
    This process results in the output of all numbers in the list that are less than 5.

---
+++ Challenge10.py
@@ -0,0 +1,16 @@
+import random
+# Initializing a list 'a'
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+ 
+# Looping through each element 'i' in the list 'a'
+for i in a:
+    if i < 5:                                       # Checking if the current element 'i' is less than 5
+        print(i) 
+
+
+
+
+#Output obtained: 1
+                  1
+                  2
+                  3
