cb84804e3523ef9a95d1beea3b02e71ce2627885
Jayalakshmy Jayakrishnan <lakshmystudy21@gmail.com>
2024-07-05 22:27:32 -0700

    Challenge11.py
    //PALINDROME//
    
    This Python code checks whether a given word(string) is a palindrome. 
    A palindrome is a word that reads the same backward as forward.
    The string is reversed using a method called 'string slicing', and is compared with the input string. If the reverse is equal to the input string, the strings are palindrome, else, they are not.
    
    Here, we get the reverse of the string using a slice operator ':', is used to extract a portion of a sequence by specifying a range of indices.
    Syntax: S[ start : stop : step ]
    If S is a string, the expression S[ start : stop : step ] returns the portion of the string from index start to index stop, at a step size step.
    Here, we are reversing the string using a step of '-1'.

---
+++ Challenge11.py
@@ -0,0 +1,11 @@
+# Prompt the user to enter a word
+a = input("Enter a word:")
+
+# Check if the word is equal to its reverse
+if a == a[ : :-1]:
+    print("Yes! It is a palindrome")
+else:
+    print("No! It is not a palindrome")
+
+
+
