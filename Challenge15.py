1d96211c8ed4c23eb21cc4f289c3d6c9a2ae7ccf
Jayalakshmy Jayakrishnan <lakshmystudy21@gmail.com>
2024-07-08 03:09:55 -0700

    Challenge15.py
    The code reads an input from the user and prints consecutive numbers from 1 to n without adding spaces or newlines in between them.
    
    Here, the if __name__ == '__main__': construct is used in Python to allow or prevent certain code from being run when the script is executed directly versus when it is imported as a module into another script. That is, this line ensures that the code block underneath it runs only if the script is executed directly (not imported as a module).
    
    The 'for' loop iterates over a range of numbers from 1 to n. 
    The range function generates numbers starting from 1 up to, but not including, n + 1.

---
+++ Challenge15.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+
+    n = int(input())                    #Read an integer input from the user
+    for i in range (1, n+1):            #Iterate through numbers from 1 to n 
+        print(i,end='')                 #Print each number without adding a newline
