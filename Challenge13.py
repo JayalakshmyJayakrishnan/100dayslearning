ae7e131a30dd614500f3fb56cd8d56d6755a454f
Jayalakshmy Jayakrishnan <lakshmystudy21@gmail.com>
2024-07-07 23:49:35 -0700

    Challenge13.py
    The code takes an integer input n and prints the squares of all integers from 0 to n-1. 
    The program uses a 'for' loop to iterate through the range of numbers, calculates the square of each number, and then prints the result.

---
+++ Challenge13.py
@@ -0,0 +1,7 @@
+#Read an integer input from the user
+n = int(input())
+
+#Loop from 0 to n-1
+for i in range(n):
+    i*i                      #Calculate the square of the current value of i
+    print(i*i)               #Print the square of the current value of i
