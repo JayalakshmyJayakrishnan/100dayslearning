31336db2ba859b92d07ef03b4527a2c37ca46d28
Jayalakshmy Jayakrishnan <lakshmystudy21@gmail.com>
2024-07-13 01:03:58 -0700

    Challenge20.py
                  //𝐌𝐞𝐦𝐨𝐫𝐲 𝐒𝐢𝐳𝐞 𝐂𝐚𝐥𝐜𝐮𝐥𝐚𝐭𝐢𝐨𝐧 𝐨𝐟 𝐓𝐮𝐩𝐥𝐞𝐬//
    This program showcases how to use the sys.getsizeof() function to calculate the memory size of different tuples.
    
    The code includes:
    Tuple1: Contains a mix of strings and integers.
    Tuple2: Contains integers only.
    Tuple3: Contains tuples of tuples, each containing an integer and a string.
    
    𝗞𝗲𝘆 𝗣𝗼𝗶𝗻𝘁𝘀:
    1.) The sys.getsizeof() function provides a comprehensive measure of an object's memory usage, including the internal overhead used by the Python interpreter.
    2.) Use sys.getsizeof() if you need a more comprehensive and realistic measure of memory usage, including Python's internal overhead.
    3.) Use __sizeof__() if you are interested in the immediate size of the object itself, excluding any interpreter overhead.
    
    In most practical scenarios, sys.getsizeof() is often the preferred choice for a more accurate representation of an object's memory footprint.
    
    
    𝐔𝐧𝐝𝐞𝐫𝐬𝐭𝐚𝐧𝐝𝐢𝐧𝐠 𝐏𝐲𝐭𝐡𝐨𝐧'𝐬 𝐈𝐧𝐭𝐞𝐫𝐧𝐚𝐥 𝐎𝐯𝐞𝐫𝐡𝐞𝐚𝐝:
    Python's internal overhead includes various components such as object headers, reference counting, type information, memory allocation management, garbage collection, and dynamic attributes. 
    This overhead ensures the flexibility, safety, and functionality of Python objects but also means that the actual memory footprint of an object can be larger than its immediate size.

--- Challenge20.py
+++ Challenge20.py
@@ -3,13 +3,13 @@ Tuple1 = ("J" ,10,  "A", 19, "Z" , 11)
 Tuple2 = (1, 2 ,3 ,4)
 Tuple3 = ((1,"Clutch") , (2,"Brake"), (3,"Accelerator"))
 
-print("Size of Tuple1: " + str(Tuple1.__sizeof__()) + "bytes")
-print("Size of Tuple2: " + str(Tuple2.__sizeof__()) + "bytes")
-print("Size of Tuple3: " + str(Tuple3.__sizeof__()) + "bytes")
+print("Size of Tuple1: " + str(sys.getsizeof(Tuple1)) + "bytes")
+print("Size of Tuple2: " + str(sys.getsizeof(Tuple2)) + "bytes")
+print("Size of Tuple3: " + str(sys.getsizeof(Tuple3)) + "bytes")
 
 
 
 
-#Output: Size of Tuple1: 72bytes
-         Size of Tuple2: 56bytes
-         Size of Tuple3: 48bytes
+# OUTPUT:  Size of Tuple1: 88bytes
+           Size of Tuple2: 72bytes
+           Size of Tuple3: 64bytes
