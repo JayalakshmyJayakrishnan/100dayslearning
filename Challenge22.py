6bef2f5aee691255f3d1c0a06ea9da71aa15d4bb
Jayalakshmy Jayakrishnan <lakshmystudy21@gmail.com>
2024-07-16 10:56:15 -0700

    Challenge22.py
                                 NUMBER GUESSING GAME
    
    This program implements a simple number guessing game where the player attempts to guess a randomly generated number between 1 and 100.
    Code in brief:
    1.) import random: Imports the random module to generate random numbers.
    
    2.) The randint() method returns an integer number selected element from the specified range.
      syntax: random.randint(start, stop)
    
    3.) while True: 
        Continuously prompts the player for guesses until the correct number is guessed.
    
    4.) The try block lets you test a block of code for errors.
          The except block lets you handle the error.
    
    5.) ValueError is raised when an object is assigned the right data type but the wrong value for a certain operation.

---
+++ Challenge22.py
@@ -0,0 +1,25 @@
+import random 
+
+
+num = random.randint(1, 100)
+tries = 0
+
+print("Guess the right number or you're stuck in this loop forever!")
+print("Think of a number which lies between 1 and 100.")
+
+while True:
+    try:
+        guess = int(input("Your guess: "))
+        tries += 1
+
+        if guess < num:
+            print("Perhaps, go for a higher number.")
+        elif guess > num:
+            print("Well, try for a lower number instead.")
+        else:
+            print(f"Luck was on your side this time! You've successfully guessed the number in {tries} tries.")
+            break
+    except ValueError:
+        print("OOPS!! Please enter a number between 1 and 100.")
+
+print("Managed to escape? Bravo! If not, keep playing.")
