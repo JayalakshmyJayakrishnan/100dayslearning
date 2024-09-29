da3759c2c06b4e2d59e5ce1516bf86522ac8bf58
Jayalakshmy Jayakrishnan <lakshmystudy21@gmail.com>
2024-09-29 11:14:59 +0530

    Challenge33.py
    In python, string . format() method provides the functionality for efficiently handling complex string substitutions and data formatting.
    The .format() method is used to format strings by inserting values into placeholders within the string. These placeholders are defined using curly braces {}.
    
    1.) Placeholders:
    The curly braces {} act as placeholders for values you want to insert into the string.
    
    2.) Arguments:
    The values to be inserted are passed as arguments to the .format() method.
    
    3.) Order:
    By default, the values are inserted into the placeholders in the order they are passed to the .format() method.
    Indexing:
    You can also use indexes within the curly braces to specify the order of the values:

---
+++ Challenge33.py
@@ -0,0 +1,5 @@
+def mult(n):
+    for i in range(1,11):
+        print("{multiplier}*{multiplicant}={multiplication}".format(multiplier=n, multiplicant=i, multiplication = n*i))
+        
+mult(10)       
